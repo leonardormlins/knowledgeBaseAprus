3caf80ed2e0fce071eb7139d324741301356ba0e
Leonardo Rodrigues Miranda Lins <leonardolins.ext@hotmail.com>
2019-09-04 20:39:18 -0300

    Register terminated

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -4,8 +4,13 @@ const User = require('..\\model\\User')
 const router = express.Router()
 
 router.post('/register', async (req, res)=>{
+    const { email } = req.body
     try{
+        if(await User.findOne({ email })){
+            return res.status(400).send({error: 'Email already exists!'})    
+        }
         const user = await User.create(req.body)
+        user.password = undefined
         return res.send({ user })
     }
     catch(err){
