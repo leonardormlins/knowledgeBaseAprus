573c2dfd81ff989c0642d188800a707911523bb6
Leonardo Rodrigues Miranda Lins <leonardolins.ext@hotmail.com>
2019-09-04 19:19:45 -0300

    MongoConnection finished!

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const User = require('..\\db');
+const User = require('..\\model\\User');
 
 const router = express.Router();
 
 router.post('/register', async (req, res)=>{
     try{
-        const user = User.create(req.body);
-        console.log(user);
-        return res.send({user});
+        const user = await User.create(req.body);
+        return res.send({ user });
     }
     catch(err){
         return res.status(400).send({error: 'Registration failed'});
--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/aprusbd',{useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/aprusbd',{useCreateIndex: true, useNewUrlParser: true})
+.then(() => console.log("Conectado ao mongo"))
+.catch(err => console.log("Falha na conexão: "+err));
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
