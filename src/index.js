fc20b14b5db820afdb8941b0444db87604e3d05d
Leonardo Rodrigues Miranda Lins <leonardolins.ext@hotmail.com>
2019-09-05 22:30:58 -0300

    Criando staticos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('static'))
 
 require("./controller/authController")(app)
 
